dd541563c75e36ebebf1c493d221c1c0eb727000
naruto2205200 <perfect_zhangxf@163.com>
2019-12-27 18:00:01 +0800

    test

--- day01/venv/day10/test.py
+++ day01/venv/day10/test.py
@@ -1,6 +1,8 @@
-lsit1 = [1,5,8,6,7,5,8,2]
-print(list(map(lambda item:item+1,lsit1)))
+list1 = [1,5,8,6,7,5,8,2]
+print(list(map(lambda item:item+1,list1)))
 
 from functools import reduce
 tuple1 = (2,5,8,6,4)
 print(reduce(lambda x,y:x+y,tuple1))
+result = filter(lambda x:x>5,list1)
+print(list(result))
